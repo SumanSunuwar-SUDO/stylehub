24b7d7079d11c62ceeb8d6f7d2e9bf6096525f0c
SumanSunuwar-SUDO <tooesa600@gmail.com>
2025-12-21 10:39:53 +0545

    Hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Hero = () => {
-  return <>This is hero section.</>;
+  return (
+    <div className="">
+      <h1 className="">Welcome to StyleHub</h1>
+
+      <div>
+        <h2 className="text-xl font-semibold mb-3">Products</h2>
+        <div>Product items</div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
