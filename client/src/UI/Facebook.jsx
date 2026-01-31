de257a4e0513a59eac0b3488d98b280f8a30d7f4
SumanSunuwar-SUDO <tooesa600@gmail.com>
2026-01-31 16:13:21 +0545

    feat: stylehub

--- client/src/UI/Facebook.jsx
+++ client/src/UI/Facebook.jsx
@@ -5,8 +5,8 @@ const Facebook = () => {
     <div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
+        width="30"
+        height="30"
         viewBox="0 0 24 24"
       >
         <path
