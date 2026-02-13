9897e53941b41f3ed3a3efca9e9658f3c12c8ac8
SumanSunuwar-SUDO <tooesa600@gmail.com>
2026-02-13 21:15:47 +0545

    feat:stylehub

--- client/src/app/admin/page.jsx
+++ client/src/app/admin/page.jsx
@@ -2,13 +2,18 @@
 
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
+import Loader from "../(main)/components/Loader";
 
 const page = () => {
   const router = useRouter();
   useEffect(() => {
     router.push("/admin/dashboard");
   }, []);
-  return <div>page</div>;
+  return (
+    <div>
+      <Loader />
+    </div>
+  );
 };
 
 export default page;
